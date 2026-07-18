abaa59c82ab332cb31dbd9faf630a8def7e24aca
Özgür <ozgurkadakal61@gmail.com>
2026-07-18 16:06:18 +0300

    DTOlar oluşturuldu

---
+++ DigitalWallet/DigitalWallet/DTO/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigitalWallet.DTO
+{
+    public class LoginDTO
+    {
+        [Required]
+        [StringLength(11, MinimumLength = 11)]
+        public string TC { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ DigitalWallet/DigitalWallet/DTO/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigitalWallet.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Surname { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(11, MinimumLength = 11)]
+        public string TC { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
