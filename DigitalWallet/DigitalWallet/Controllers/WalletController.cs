f9d8ecfa4f39f50b2118ed9c063d29fc07c473af
Özgür <ozgurkadakal61@gmail.com>
2026-07-21 13:01:20 +0300

    WalletControlleri oluşturdum ve içine bakiye gösterme endpointini ekledim. Sırada diğer endpointler var.

---
+++ DigitalWallet/DigitalWallet/Controllers/WalletController.cs
@@ -0,0 +1,44 @@
+﻿using DigitalWallet.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DigitalWallet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WalletController : ControllerBase
+    {
+        private readonly IWalletService _walletService;
+
+        public WalletController(IWalletService walletService)
+        {
+            _walletService = walletService;
+        }
+
+        [HttpGet("balance")]
+        public async Task<IActionResult> GetBalance()
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userIdString))
+            {
+                return Unauthorized("Kimlik doğrulama hatası! Geçersiz Token.");
+            }
+
+            int userId = int.Parse(userIdString);
+
+            var result = await _walletService.GetBalance(userId);
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+
+        }
+    }
+}
